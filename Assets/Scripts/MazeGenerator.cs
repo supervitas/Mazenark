8302892743092cb527a6897265a5ca9896e183e8
Paul Maminov <xobotun@inbox.ru>
2017-01-28 22:21:22 +0300

    Now with 10 more program-defined blocks!

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour {
+
+    [Tooltip("Object to be spawned as maze blocks")]
+    public GameObject prefab;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+            Instantiate(prefab, new Vector3(i * 10.0f, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
