b27be1addb4bc63242b0640594cff5f494cd96cc
Paul Maminov <xobotun@inbox.ru>
2017-01-31 12:55:09 +0300

    Fucked with motion. IMO, previous version was better, but this "uses physics"! :(

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 15000.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 15000.0f;
+
+        //GetComponent<Rigidbody>().AddTorque(new Vector3(0, x, 0));
+        GetComponent<Rigidbody>().AddForce(new Vector3(x, 0, z));
+	}
+}
