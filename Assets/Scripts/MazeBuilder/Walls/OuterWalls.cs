5001cf39eff4ff15e80a51a8eb80808feb11e304
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-02-15 02:03:52 +0300

    4 walls. Bad code.Need refactor

--- Assets/Scripts/MazeBuilder/Walls/OuterWalls.cs
+++ Assets/Scripts/MazeBuilder/Walls/OuterWalls.cs
@@ -1,4 +1,5 @@
-﻿using Constants;
+﻿using System.Runtime.InteropServices;
+using Constants;
 using UnityEngine;
 
 namespace Walls {
@@ -6,16 +7,22 @@ namespace Walls {
     public class OuterWalls : MonoBehaviour {
         [Tooltip("Maze Walls prefab")]
         public GameObject walls;
+        private readonly MazeSizeGenerator _mazeSize = MazeSizeGenerator.Instance;
+
+        private void generateWall(float x, int y, float z, int rotation) {
+            var wall = Instantiate(walls, new Vector3(x, y,
+                z), Quaternion.Euler(0, rotation, 0));
+            wall.transform.localScale = new Vector3(20, _mazeSize.Y, _mazeSize.Y * 10);
+            wall.GetComponent<Renderer>();
+        }
 
         private void Start() {
-            var mazeSize = MazeSizeGenerator.Instance;
-            for (var i = 0; i < 1; i++) {
-                var wall = Instantiate(walls, new Vector3(-10, 32,
-                    (float) (mazeSize.Y * 4.5)), Quaternion.Euler(0, 0, 0));
-                wall.transform.localScale = new Vector3(10, mazeSize.Y, mazeSize.Y * 10);
-                wall.GetComponent<Renderer>();
+            generateWall(x: -10, y: 32, z: (float) (_mazeSize.Y * 4.5), rotation: 0);
+            generateWall(x: (float) (_mazeSize.X * 5), y: _mazeSize.Y / 2, z: -(_mazeSize.Y / 2), rotation: 90);
+            generateWall(x: _mazeSize.X * 10, y: 32, z: (float) (_mazeSize.Y * 4.5), rotation: 0);
+            generateWall(x: (float) (_mazeSize.X * 5), y: _mazeSize.Y / 2, z: (_mazeSize.Y * 10) - 20 , rotation: 90);
 
-            }
+
 
         }
 
