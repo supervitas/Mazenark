2a15a529d36847da3dfda1747c128573f2c43efe
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-03-14 22:19:21 +0300

    some navmesh

---
+++ Assets/Scripts/InGameNavigation/NavMeshPathWallker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace InGameNavigation {
+    public class NavMeshPathWallker : MonoBehaviour {
+
+        public Transform goal;
+
+        void Start() {
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.destination = goal.position;
+        }
+
+
+    }
+}
