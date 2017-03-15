6701b17b97000cfd60bcdbd3ebf181e8f839a915
Paul Maminov <xobotun@inbox.ru>
2017-03-15 20:33:47 +0300

    Now it is companion cube!

--- Assets/Scripts/InGameNavigation/NavMeshPathWallker.cs
+++ Assets/Scripts/InGameNavigation/NavMeshPathWallker.cs
@@ -3,18 +3,26 @@ using UnityEngine.AI;
 
 namespace InGameNavigation {
     public class NavMeshPathWallker : MonoBehaviour {
+		
 
         public Transform goal;
+		public float distanceWhereToStop = 3.0f;
 
-        void Start () {
+		void Start () {
             NavMeshAgent agent = GetComponent<NavMeshAgent>();
             agent.destination = goal.position;
         }
 
         private void Update() {
             NavMeshAgent agent = GetComponent<NavMeshAgent>();
-            agent.destination = goal.position;
 
-        }
+			if ((agent.transform.position - goal.position).magnitude < distanceWhereToStop) {
+				agent.destination = agent.transform.position;
+				return;
+			}
+
+			agent.destination = goal.position;
+
+		}
     }
 }
