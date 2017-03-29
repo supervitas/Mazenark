d6e176261a7f470acd2808c54268d50a28abee7d
Paul Maminov <xobotun@inbox.ru>
2017-03-29 21:18:37 +0300

    Moved Edge into separate subclass.

--- Assets/Scripts/MazeBuilder/BiomeGenerators/EarthGenerator.cs
+++ Assets/Scripts/MazeBuilder/BiomeGenerators/EarthGenerator.cs
@@ -90,21 +90,4 @@ namespace MazeBuilder.BiomeGenerators {
 
     }
 
-
-    internal class Edge {
-        public static List<Edge> Edges = new List<Edge>();
-        public Quaternion Rotation {
-            get; private set;
-        }
-        private Edge(Quaternion rotation) {
-            Rotation = rotation;
-            Edges.Add(this);
-        }
-
-        public static Edge UpRight = new Edge(Quaternion.Euler(-90, 90, 0));
-        public static Edge UpLeft = new Edge(Quaternion.Euler(-90, 180, 0));
-        public static Edge DownLeft = new Edge(Quaternion.Euler(-90, 270, 0));
-        public static Edge DownRight = new Edge(Quaternion.Euler(-90, 360, 0));
-
-    }
 }
---
+++ Assets/Scripts/MazeBuilder/Utility/Edge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MazeBuilder.Utility {
+	internal class Edge {
+		public static List<Edge> Edges = new List<Edge>();
+
+		public Quaternion Rotation {
+			get; private set;
+		}
+
+		private Edge(Quaternion rotation) {
+			Rotation = rotation;
+			Edges.Add(this);
+		}
+
+		public static Edge UpRight = new Edge(Quaternion.Euler(-90, 90, 0));
+		public static Edge UpLeft = new Edge(Quaternion.Euler(-90, 180, 0));
+		public static Edge DownLeft = new Edge(Quaternion.Euler(-90, 270, 0));
+		public static Edge DownRight = new Edge(Quaternion.Euler(-90, 360, 0));
+	}
+}
