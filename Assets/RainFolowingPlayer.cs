514bd6b82e3b2d7b6f524fa036e5ba95ac5dee6c
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-03-31 10:55:25 +0300

    simple rain

---
+++ Assets/RainFolowingPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainFolowingPlayer : MonoBehaviour {
+
+    [SerializeField] private Transform _target;
+
+    private Vector3 _position = new Vector3();
+	// Use this for initialization
+	void Start () {
+	    InvokeRepeating("UpdateRainPosition", 0, 2);
+	}
+	
+	// Update is called once per frame
+    private void UpdateRainPosition() {
+        _position.Set(_target.position.x, 0, _target.position.z);
+        transform.SetPositionAndRotation(_position, Quaternion.identity );
+    }
+}
--- Assets/Scripts/MazeBuilder/BiomeGenerators/WaterGenerator.cs
+++ Assets/Scripts/MazeBuilder/BiomeGenerators/WaterGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace MazeBuilder.BiomeGenerators {
     public class WaterGenerator : AbstractBiomeGenerator {
+        [Header("Rain Effect")]
+        public GameObject Rain;
+
 
         private new void Awake() {
             base.Awake();
@@ -30,10 +33,18 @@ namespace MazeBuilder.BiomeGenerators {
         }
         protected override void StartPostPlacement(object sender, EventArguments e) {
             PlaceLightingObjects();
+//            PlaceRain();
         }
 
         private void PlaceLightingObjects() {
             ParticleList = PlaceLightingParticles(Biome.Water, NightParticles);
+        }
+
+        private void PlaceRain() {
+            var emptyTiles = GetTilesByTypeAndBiome(Biome.Water, Tile.Variant.Empty);
+            foreach (var tile in emptyTiles) {
+                Instantiate(Rain, GetDefaultPositionVector(tile.Position, 0.1f), Quaternion.identity);
+            }
         }
 
         public override void CreateWall(Biome biome, Coordinate coordinate, Maze maze) {
