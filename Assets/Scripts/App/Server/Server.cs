f9130d4b3807064db13c14a39052669884651682
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-04-04 21:00:50 +0300

    monsters + chests

--- Assets/Scripts/App/Server/Server.cs
+++ Assets/Scripts/App/Server/Server.cs
@@ -7,6 +7,13 @@ namespace App.Server {
             AppManager.Instance.MazeSize.GenerateRndSize();
             AppManager.Instance.MazeInstance = new MazeBuilder.MazeBuilder(AppManager.Instance.MazeSize.X, AppManager.Instance.MazeSize.Y);
             AppManager.Instance.EventHub.CreateEvent("MazeCreated", null);
+
+        }
+        public struct MazeStruct {
+            public int X;
+            public int Y;
+            public string BiomeName;
+            public int TileType;
         }
 
     }
