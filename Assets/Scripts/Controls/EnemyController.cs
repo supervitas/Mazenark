8ccfe2d4869e8b5f9c9d4d129c7500f90b091c4d
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-04-12 19:17:48 +0300

    navmesh to enemy

--- Assets/Scripts/Controls/EnemyController.cs
+++ Assets/Scripts/Controls/EnemyController.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Controls {
-    public class EnemyController : MonoBehaviour {
+    public class EnemyController : NetworkBehaviour {
         [SerializeField] private Animator m_animator;
 
         // Use this for initialization
         void Start () {
+            Debug.Log(transform.position.x);
             m_animator.SetBool("Idle", true);
         }
 
