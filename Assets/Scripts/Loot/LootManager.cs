ebd4544504a40d2f44ac5422a429cb3c4ff1005c
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-05-16 13:20:21 +0300

    moved loot manager to game scene

--- Assets/Scripts/Loot/LootManager.cs
+++ Assets/Scripts/Loot/LootManager.cs
@@ -14,15 +14,6 @@ namespace Loot {
 		[SerializeField]
 		private GameObject lootObject;
 
-		private void Awake() {
-			if (!Instance) {
-				Instance = this;
-			} else {
-				Destroy(gameObject);
-			}
-			DontDestroyOnLoad(gameObject);
-		}
-
 		public void CreateLoot(Vector3 where) {
 			if (Random.Range(0, 101) <= chanceOfSpawnLoot) {
 				var instantiated = Instantiate(lootObject, where, Quaternion.identity);
