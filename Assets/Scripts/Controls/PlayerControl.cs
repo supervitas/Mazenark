91366a51725ac6106c26797ba703ae6d7564d5b8
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-08-01 21:18:54 +0300

    added teleport to spawnable

--- Assets/Scripts/Controls/PlayerControl.cs
+++ Assets/Scripts/Controls/PlayerControl.cs
@@ -15,11 +15,6 @@ namespace Controls {
     [NetworkSettings(channel = 1, sendInterval = 0.2f)]
     public class PlayerControl : NetworkBehaviour {
 
-        private enum ControlMode {
-            Tank,
-            Direct
-        }
-        
         [SyncVar(hook = nameof(OnSetName))] 
         private string _playerName;
         
@@ -32,8 +27,7 @@ namespace Controls {
         [SerializeField] private Animator m_animator;
         [SerializeField] private Rigidbody m_rigidBody;
         [SerializeField] private GameObject spellCastEffect;
-
-        [SerializeField] private ControlMode m_controlMode = ControlMode.Direct;
+       
 
         public GameObject PlayerCamera;
         private Camera _camera;       
@@ -46,8 +40,8 @@ namespace Controls {
         private float _castTime;
         private float _timeCasted;
         private float _timeCooled;
-        private float _coolDown = 0.2f;
-        
+        private const float _coolDown = 0.2f;
+
         private readonly Vector3 _zeroVector = Vector3.zero;
         
         
@@ -216,20 +210,9 @@ namespace Controls {
             _timeCasted = 0;
             _uiSpellCast.Reset();
             _spellEffect.SetActive(false);
+         
+            TankUpdate();                    
 
-            switch (m_controlMode) {
-                case ControlMode.Direct:
-                    DirectUpdate();
-                    break;
-
-                case ControlMode.Tank:
-                    TankUpdate();
-                    break;
-
-                default:
-                    Debug.LogError("Unsupported state");
-                    break;
-            }
 
             m_wasGrounded = m_isGrounded;
         }
@@ -253,35 +236,7 @@ namespace Controls {
 
             JumpingAndLanding();
         }
-
-        private void DirectUpdate() {
-            float v = Input.GetAxis("Vertical");
-            float h = Input.GetAxis("Horizontal");
-
-            Transform camera = _camera.transform;
-  
-
-            m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
-            m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
-
-            Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
-
-            float directionLength = direction.magnitude;
-            direction.y = 0;
-            direction = direction.normalized * directionLength;
-
-            if (direction != _zeroVector) {
-                m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
-
-                transform.rotation = Quaternion.LookRotation(m_currentDirection);
-                transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
-
-                m_animator.SetFloat("MoveSpeed", direction.magnitude);
-            }
-
-            JumpingAndLanding();
-        }
-
+        
         private void JumpingAndLanding() {
             bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
 
