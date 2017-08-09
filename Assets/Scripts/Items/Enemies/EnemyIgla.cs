629278bb9820472a7637b8ba82a03faba9a680ed
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-08-10 02:25:27 +0300

    igla for bats

---
+++ Assets/Scripts/Items/Enemies/EnemyIgla.cs
@@ -0,0 +1,26 @@
+﻿using CharacterControllers;
+using UnityEngine;
+
+namespace Items.Enemies {
+    public class EnemyIgla : Weapon {
+        
+        protected override void Start() {}
+
+        void OnCollisionEnter(Collision other) {
+            var go = other.gameObject;            
+            if (go.CompareTag("Player")) {                
+                go.GetComponent<ServerCharacterController>().TakeDamage(100, 3.5f);   
+            }            
+            Destroy(this);
+        }
+                
+
+        public void OnDestroy() {                      
+            Destroy(gameObject, 1f);
+        }
+
+        public override void Fire() {            
+            gameObject.GetComponent<Rigidbody>().velocity = gameObject.transform.forward * 50;
+        }
+    }
+}
