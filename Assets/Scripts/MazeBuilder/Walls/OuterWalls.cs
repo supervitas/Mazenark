cb15672ff47523011ee0812c442e4a75b246538a
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-08-27 00:58:16 +0300

    bump

--- Assets/Scripts/MazeBuilder/Walls/OuterWalls.cs
+++ Assets/Scripts/MazeBuilder/Walls/OuterWalls.cs
@@ -26,16 +26,16 @@ namespace MazeBuilder.Walls {
             var mazeSize = AppManager.Instance.MazeInstance;
 
             GenerateWall(mazeSize.Width * Constants.Maze.TILE_SIZE + 25, Quaternion.Euler(0, 90, 0),
-                getPosition: index => new Vector3(-25, 0, index), increment: bounds => bounds.z); // Left
+                getPosition: index => new Vector3(-25, -8, index), increment: bounds => bounds.z); // Left
 
             GenerateWall(mazeSize.Height * Constants.Maze.TILE_SIZE + 25, Quaternion.identity,
-                getPosition: index => new Vector3(index, 0, -25), increment: bounds => bounds.x); // Bottom
+                getPosition: index => new Vector3(index, -8, -25), increment: bounds => bounds.x); // Bottom
 
             GenerateWall(mazeSize.Width * Constants.Maze.TILE_SIZE + 25, Quaternion.Euler(0, 270, 0),
-                getPosition: index => new Vector3(mazeSize.Width * 8 + 25, 0, index), increment: bounds => bounds.z); // Right
+                getPosition: index => new Vector3(mazeSize.Width * 8 + 25, -8, index), increment: bounds => bounds.z); // Right
 
             GenerateWall(mazeSize.Height * Constants.Maze.TILE_SIZE + 25, Quaternion.Euler(0, 180, 0),
-                getPosition: index => new Vector3(index, 0, mazeSize.Height * 8 + 25), increment: bounds => bounds.x); // Top
+                getPosition: index => new Vector3(index, -8, mazeSize.Height * 8 + 25), increment: bounds => bounds.x); // Top
 
         }
 
---
+++ Assets/Scripts/Ui/GameMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Ui {
+    public class GameMenu : MonoBehaviour {
+        
+        public void ShowMenu() {
+                
+        }
+    }
+}
