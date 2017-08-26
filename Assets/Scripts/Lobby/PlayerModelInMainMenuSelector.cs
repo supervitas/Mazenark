7890287c37cfc8e781a36c33404c28f070be9656
Paul Maminov <xobotun@inbox.ru>
2017-08-26 21:02:21 +0300

    Better lobby in two more commits!

---
+++ Assets/Scripts/Lobby/PlayerModelInMainMenuSelector.cs
@@ -0,0 +1,23 @@
+﻿using MazeBuilder.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModelInMainMenuSelector : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+	public GameObject player3;
+
+	void Start () {
+		var playerModels = new CollectionRandom();
+		playerModels.Add(player1, typeof(GameObject));
+		playerModels.Add(player2, typeof(GameObject));
+		playerModels.Add(player3, typeof(GameObject));
+
+		var playerModel = (GameObject) playerModels.GetRandom(typeof(GameObject));
+
+		Instantiate(playerModel, this.transform.position, Quaternion.Euler(0, 180, 0));
+	}
+
+}
